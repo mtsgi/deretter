8414ea6acd094ebf1d7aec2f4ba9bc74d84734ce
mtsgi <oq@live.jp>
2021-12-27 23:57:30 +0900

    Init nuxt3

--- src/views/About.vue
+++
@@ -1,6 +0,0 @@
-<template>
-  <div class="about">
-    <h1>About</h1>
-    <p v-text="$root.config"></p>
-  </div>
-</template>
--- src/views/Hashtag.vue
+++
@@ -1,6 +0,0 @@
-<template>
-  <div class="hashtag">
-    <h1>ハッシュタグ検索結果</h1>
-    <p>{{ $route.params.id }}</p>
-  </div>
-</template>
